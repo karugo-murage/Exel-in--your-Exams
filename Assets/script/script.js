04e4fc134d920b06b90adcaa574c4b8c00247c5d
karugo-murage <murage.gidion@moringaschool.com>
2022-06-24 23:52:58 +0300

    add ai event-listener

---
+++ Assets/script/script.js
@@ -0,0 +1,18 @@
+const ai = document.querySelector('.ai')
+ai.addEventListener('click', function(){
+    let unitImage = document.querySelector('.exams')
+    let mySrc = unitImage.getAttribute('src')
+    if (mySrc !== "Assets/images/ai.jpg"){
+        unitImage.setAttribute('src', "Assets/images/ai.jpg")
+    }
+
+})
+// const calculus = document.querySelector('.calculus')
+// calculus.addEventListener('click', function(){
+//     let unitImage = document.querySelector('.exams')
+//     let mySrc = unitImage.getAttribute('src')
+//     if (mySrc !== "Assets/images/calculus.jpg"){
+//         unitImage.setAttribute('src', "Assets/images/calculus.jpg")
+//     }
+
+// })
